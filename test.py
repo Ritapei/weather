56d0dfe47b81259f6973a25a0a04616839efa59c
Ritapei <106952378+Ritapei@users.noreply.github.com>
2022-06-23 17:44:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,11 +11,9 @@ for i in area:
     url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-{}?Authorization=CWB-40C73C51-9AF7-4B8B-A5BE-8635FB67523F&format=JSON'.format(i)
     data = requests.get(url).json()
     information_list.append(data)
-cred = credentials.Certificate('serviceAccount.json')
+cred = credentials.Certificate("serviceAccount.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-with open(information_list, 'r') as j:
-        contents = json.loads(j.read())
 for y in information_list:
     doc_ref = db.collection(str(datetime.date.today())).document(y["records"]["locations"][0]["locationsName"])
     doc_ref.set({"地區":y["records"]["locations"][0]["location"]})
